e66d32d1ffb2c0e2514b8a5b774679796cd890e7
Kesney Saurel <kesney095@gmai.com>
2024-07-06 17:17:49 -0500

    Added more tickers and more range for precise alerts

--- main.py
+++ main.py
@@ -26,11 +26,16 @@ def scan(ticker):
                       params={'symbol': ticker['symbol'], 'interval': 'daily', 'start': yt_date, 'end': date.today(), 'session_filter': 'open'},
                       headers={'Authorization': 'Bearer '+api_key, 'Accept': 'application/json'} )
         pd_json = pd.json()
-        current_stock_price = ticker['price']
-        yt_low              = pd_json["history"]["day"]['low']
-        #check if the Stock is currently $1 away from the PDL 
-        if (float(current_stock_price) - float(yt_low) <= 1):
-            print("Send Alert") 
+        current_stock_price = float(ticker['price'])
+        yt_low              = float(pd_json["history"]["day"]['low'])
+        #print(current_stock_price)
+        #Check if the stock price is less than 200 and currently $1 away from the PDL, then Alert
+        if ( current_stock_price <= 200 and (current_stock_price - yt_low) <= 1):
+            print(f"Send Alert - {ticker['symbol']}")
+        elif (current_stock_price > 200 and  current_stock_price < 400 and (current_stock_price - yt_low) <= 1.5 ):
+            print ("200-400 Alert ")
+        elif(current_stock_price > 400 and (current_stock_price - yt_low) <= 30):
+            print (f" 400 + {ticker['symbol']}")
     except :
         print (f"Error Occured while fetching daily data for  {ticker['symbol']}")
 
@@ -42,7 +47,7 @@ def scan(ticker):
 async def ws_connect():
     uri = "wss://ws.tradier.com/v1/markets/events"
     async with websockets.connect(uri, ssl=True, compression=None) as websocket:
-        payload = '{"symbols": ["AAPL","AMZN","TSLA","GOOGL","RDDT","RIVN","FDX"], "sessionid": "'+ SESSION_ID +'", "filter": ["trade"], "linebreak": true}'
+        payload = '{"symbols": ["AAPL","AMZN","TSLA","GOOGL","RDDT","RIVN","FDX", "SMCI"], "sessionid": "'+ SESSION_ID +'", "filter": ["trade"], "linebreak": true}'
         await websocket.send(payload)
 
         #print(f">>> {payload}")
