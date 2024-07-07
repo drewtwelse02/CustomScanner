ee2d032fdee78ed85441cd8e4c6c96357bb34c5c
Kesney Saurel <kesney095@gmai.com>
2024-07-06 23:36:25 -0500

    More Tickers

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from datetime import date, timedelta
 historical_data_url = "https://api.tradier.com/v1/markets/history"
 session_auth_url    = "https://api.tradier.com/v1/markets/events/session"
 
+
+Stock_List = ["AAPL","GOOGL","RDDT","MSTR","MARA","COIN","MU","QCOM","AMD","AVGO","NVDL","SMCI","TSLA","RIVN","WFC","GS","BOFA","AXP","MS","JPM","FDX","UPS","AMZN"]
+
 # Create Market Session 
 api_key = os.environ['TRADIER_API_KEY']
 response = requests.post(session_auth_url,
@@ -28,32 +31,28 @@ def scan(ticker):
         pd_json = pd.json()
         current_stock_price = float(ticker['price'])
         yt_low              = float(pd_json["history"]["day"]['low'])
-        #print(current_stock_price)
         #Check if the stock price is less than 200 and currently $1 away from the PDL, then Alert
         if ( current_stock_price <= 200 and (current_stock_price - yt_low) <= 1):
-            print(f"Send Alert - {ticker['symbol']}")
+            print (f"Send Alert (1 + ) - {ticker['symbol']}")
         elif (current_stock_price > 200 and  current_stock_price < 400 and (current_stock_price - yt_low) <= 1.5 ):
-            print ("200-400 Alert ")
-        elif(current_stock_price > 400 and (current_stock_price - yt_low) <= 30):
-            print (f" 400 + {ticker['symbol']}")
+            print (f"Send Alert (200 + ) - {ticker['symbol']}")
+        #SMCI AVGO ETC 
+        elif(current_stock_price > 400 and (current_stock_price - yt_low) <= 5):
+            print (f"Send Alert (400+) - {ticker['symbol']}")
     except :
         print (f"Error Occured while fetching daily data for  {ticker['symbol']}")
 
     
 
-    
-
 #Connect to the web socket Server 
-async def ws_connect():
+async def ws_connect(sl):
     uri = "wss://ws.tradier.com/v1/markets/events"
     async with websockets.connect(uri, ssl=True, compression=None) as websocket:
-        payload = '{"symbols": ["AAPL","AMZN","TSLA","GOOGL","RDDT","RIVN","FDX", "SMCI"], "sessionid": "'+ SESSION_ID +'", "filter": ["trade"], "linebreak": true}'
+        payload = '{"symbols": ["AAPL","GOOGL","RDDT","MSTR","MARA","COIN","MU","QCOM","AMD","AVGO","NVDL","SMCI","TSLA","RIVN","WFC","GS","BOFA","AXP","MS","JPM","FDX","UPS","AMZN"], "sessionid": "'+ SESSION_ID +'", "filter": ["trade"], "linebreak": true}'
         await websocket.send(payload)
-
-        #print(f">>> {payload}")
         async for ticker in websocket:
             #print(f"<<< {ticker}")
             # Multiple Threads 
             scan(json.loads(ticker))
 
-asyncio.run(ws_connect())
+asyncio.run(ws_connect(Stock_List))
