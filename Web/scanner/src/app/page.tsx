1c65ed4f0eb7dc678c6e69e115de2c4838af7f8b
kesney095 <kesney095@gmail.com>
2024-07-23 21:13:23 -0500

    Added the web app

---
+++ Web/scanner/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="container">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, sit
+            accusamus tenetur iure laboriosam ab dolor autem mollitia perspiciatis
+            enim est sint facere temporibus tempora dolorem. Laborum veniam
+            officiis eligendi.
+          </p>
+        </div>
+
+    );
+  }
