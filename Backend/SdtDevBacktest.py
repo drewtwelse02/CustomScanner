15f49718e59bbe9e57b87ca785d123604bf6d2de
kesney095 <kesney095@gmail.com>
2024-07-25 22:19:04 -0500

    Fixed Errorss when fetching data for the weekends

--- Backend/SdtDevBacktest.py
+++ Backend/SdtDevBacktest.py
@@ -7,10 +7,7 @@ import statistics
 
 historical_data_url = "https://api.tradier.com/v1/markets/history"
 session_auth_url    = "https://api.tradier.com/v1/markets/events/session"
-Stock_List = ["SPY","QQQ","META","RDDT","MSTR","MARA","COIN","MU","QCOM","AMD","AVGO","NVDL","SMCI","TSLA","RIVN","WFC","GS","BOFA","AXP","MS","JPM","FDX","UPS","AMZN"]
-
-# Create Market Session 
-api_key = os.environ['TRADIER_API_KEY']
+Stock_List = ["QQQ","SPY","META","RDDT","MSTR","MARA","COIN","MU","QCOM","AMD","AVGO","NVDL","SMCI","TSLA","RIVN","WFC","GS","BOFA","AXP","MS","JPM","FDX","UPS","AMZN"]
 
 # Polygon API setup 
 client = RESTClient(api_key=os.environ['POLY_API_KEY'])
@@ -33,18 +30,24 @@ def backtest(tickers):
     while i < 10:    
         # Calculate the Std deviation of the last 10 days of 2mns bar data      
         td_date = td_date -timedelta(days=1)
-        aggs = []
+        aggs = [] 
         std_dev = 0
-        for a in client.list_aggs(ticker=tickers[1], multiplier=2, timespan="minute", from_=str(td_date-timedelta(days=10)), to=str(td_date), limit=50000):
-            #print("Working")
+        #Get Daily data 10 days prior yesterday 
+        for a in client.list_aggs(ticker=tickers[0], multiplier=2, timespan="minute", from_=str(td_date-timedelta(days=10)), to=str(td_date), limit=50000):
+            # Store each 2mns volume bar in an []
             aggs.append(a.volume)
         std_dev = statistics.stdev(aggs)
         #print(f"Standard Deviation of sample is {std_dev}")
         # Compare each 2mns bar of data with the std dev 
-
-        for d in client.list_aggs(ticker=tickers[1], multiplier=2, timespan="minute", from_=str(chk_date), to=str(chk_date), limit=50000):
-            if (d.volume > 5* std_dev ):
-                print(f"{std_dev}- High std dev found {convert_date(d.timestamp)} ")
+        try :
+            for d in client.list_aggs(ticker=tickers[0], multiplier=2, timespan="minute", from_=str(chk_date), to=str(chk_date), limit=50000):
+                if (d.volume > 1.5* std_dev ):
+                    converted_date = convert_date(d.timestamp)
+                    if converted_date is not None:
+                        print(f"{std_dev}- High std dev found {converted_date} ")
+        except Exception as error :
+            # Weekends 
+            w=1 # Dummy Var 
 
         td_date  = td_date-timedelta(days=1)
         chk_date = chk_date-timedelta(days=1)
