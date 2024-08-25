7d4028845b912eae42c3783e50e9f9010df76d31
drewtwelse02 <kesney095@gmail.com>
2024-08-24 21:16:13 -0500

    Added std deviation function and table to db

--- Backend/Db.py
+++ Backend/Db.py
@@ -1,12 +1,14 @@
 import sqlite3
 import DisplayColor as printinfo
 import Session
-
+import statistics
+from datetime import date
 class DB:
     def __init__(self):
         printinfo.info("Checking Database Status")
         #Check if the database already exist, If not create it 
         self.conn = sqlite3.connect("cs.db")
+        self.s = Session.Session()
         # Intialize the strategy tables 
         #self.initialize_pdl_tb ()
 
@@ -16,17 +18,19 @@ class DB:
         if (res.fetchone() is None):
             printinfo.info("Tables do not exist, creating them ...")
             # Create the new Table for prior day data (OHLC)
-            res_pd = self.conn.execute ("CREATE TABLE PD (ticker_symbol,date, pdo_price, pdl_price, pdh_price,pdc_price)")
+            res_pd = self.conn.executescript ("""
+                                              CREATE TABLE PD (ticker_symbol,date, pdo_price, pdl_price, pdh_price,pdc_price);
+                                              CREATE TABLE STDDEV (ticker_symbol,date,std_dev);
+                                              """)
             # Return True if the table was created 
             return True
         else:
             printinfo.info("PD Table Already created...")
             return False 
     async def load_last_pd_data(self,stock_list):
-        printinfo.warning("Loading Prior day Data ")
-        s = Session.Session()
+        printinfo.info("Loading Prior day Data ")
         for ticker in stock_list:
-            d_info = await s.get_yt_daily_data(ticker)
+            d_info = await self.s.get_yt_daily_data(ticker)
             #print(d_info['history']['day']['open'])
             data = [ticker,d_info['history']['day']['date'],d_info['history']['day']['open'],d_info['history']['day']['low'],d_info['history']['day']['high'],d_info['history']['day']['close']]
             self.conn.execute("INSERT INTO PD(ticker_symbol,date,pdo_price,pdl_price,pdh_price,pdc_price) VALUES (?,?,?,?,?,?)",data)
@@ -35,6 +39,22 @@ class DB:
         r = self.conn.execute("SELECT * FROM PD")
         if (r.fetchone is not None):
             printinfo.info("Prior Days Data Loaded Successfully")
+        
+    def load_std_Dev (self,stock_list):
+        printinfo.info("Loading Std Dev ... ")
+        for ticker in stock_list : 
+            vol_aggs = self.s.get_last_volume_bars(ticker)
+            std_dev = statistics.stdev(vol_aggs)
+            #print( ticker + " STD Dev is " + str(std_dev))
+            data = [ticker,date.today(),std_dev]
+            self.conn.execute("INSERT INTO STDDEV (ticker_symbol,date,std_dev) VALUES (?,?,?)",data)
+            self.conn.commit()
+        
+        r = self.conn.execute("SELECT * FROM STDDEV")
+        if (r.fetchone is not None):
+            #print(r.fetchall())
+            printinfo.info("Prior Days Data Loaded Successfully")
+
 
 
  
--- Backend/PDLScanner.py
+++ Backend/PDLScanner.py
@@ -12,14 +12,15 @@ from Db import DB
 Stock_List = ["AAPL","GOOGL","RDDT","MSTR","MARA","COIN","MU","QCOM","AMD","AVGO","NVDL","SMCI","TSLA","RIVN","WFC","GS","BAC","AXP","MS","JPM","FDX","UPS","AMZN"]
 
 # Initialize Market Session from Tradier 
-tradier_session = Session()
-session_id =tradier_session.get_session_id()
+#tradier_session = Session()
+#session_id =tradier_session.get_session_id()
 
 # Launch Database Engine 
 db = DB()
 # Check if PD table needs to be loaded 
 if (db.initialize_pd_tb() is True ):
     asyncio.run(db.load_last_pd_data(Stock_List))
+    db.load_std_Dev(Stock_List)
 
 
 
